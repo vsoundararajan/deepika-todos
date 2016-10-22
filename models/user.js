3b5c7dabe1af3f4244d74a92bb491a412b35e9e5
Soundararajan <soundararajan@usa.net>
2016-10-22 13:11:48 -0700

    Convert email to lowercase and trim

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 /**
  * Created by soundararajanvenkatasubramanian on 10/22/16.
  */
+var _ = require('lodash');
+
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('user', {
         email: {
@@ -18,5 +20,13 @@ module.exports = (sequelize, DataTypes) => {
                 len: [7, 100]
             }
         }
-    })
+    }, {
+       hooks: {
+           beforeValidate: (user, options) => {
+               if(_.isString(user.email) && user.email.length > 0){
+                   user.email = user.email.toLowerCase().trim();
+               }
+           }
+       }
+    });
 }
